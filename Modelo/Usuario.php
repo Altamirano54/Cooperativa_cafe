ad31a331518e193218a76767291bcd8b0842709c
cristian lenin campos marco <lenin18052001@gmail.com>
2025-07-08 15:49:23 -0500

    errores de login

--- Modelo/Usuario.php
+++ Modelo/Usuario.php
@@ -6,8 +6,8 @@ require_once __DIR__ . '/../Config/conexion.php';
 class Usuario {
     public static function autenticar($usuario, $password) {
         $con = Conexion::getConexion();
-        $usuario = $con->real_escape_string($usuario);
-        $password = md5($password); // O usa password_hash si prefieres más seguridad
+        $usuario = $usuario;//$con->real_escape_string($usuario);
+        $password = $password;//md5($password); // O usa password_hash si prefieres más seguridad
 
         $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND contraseña = '$password'";
         $res = $con->query($sql);
